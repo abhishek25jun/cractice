a30fa1c55768cedbe635187c968a6b4b01296322
Abhishek Sharma <sharma_abhishek@projectara.com>
2017-02-08 15:26:47 +0530

    pcs first commit

---
+++ src/main.c
@@ -0,0 +1,21 @@
+/**************************************
+ *
+ *
+ * ************************************/
+
+#include <stdio.h>
+
+void help()
+{
+	return;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2) {
+		help();
+		return 1;
+	}
+	
+
+}
